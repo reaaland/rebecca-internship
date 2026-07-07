a28f4ce5a86796ecd61ccc783fc93f38f313d7c3
Rebecca Aaland <reaaland@gmail.com>
2026-07-07 00:05:56 -0500

    getting the href=changed to react/vercel friendly language

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -134,13 +134,15 @@ const ExploreItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="https://facebook.com" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
-                        <a href="" target="_blank" rel="noreferrer">
+
+                        <a href="https://twitter.com" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="">
+
+                        <a href="mailto:">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>

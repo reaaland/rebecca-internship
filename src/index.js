ec18d385c6f1d52020450b3c7baecaf18560ed96
Rebecca Aaland <reaaland@gmail.com>
2026-08-07 22:54:49 -0500

    style: load NFT portfolio polish

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './css/portfolio-polish.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "slick-carousel/slick/slick.css";
